691ae765cecbe4e24231ab48784f811a9a0afecc
moli <2804660647@qq.com>
2024-02-23 23:53:39 +0800

    策略模式

---
+++ src/com/moli/strategy/Duck.java
@@ -0,0 +1,14 @@
+package com.moli.strategy;
+
+/**
+ * @author moli
+ * @time 2024-02-23 23:16:38
+ * @description 鸭子
+ */
+public abstract class Duck {
+
+    public void quack() {
+        System.out.println("鸭子叫");
+    }
+
+}
---
+++ src/com/moli/strategy/StrategyPattern.java
@@ -0,0 +1,14 @@
+package com.moli.strategy;
+
+/**
+ * @author moli
+ * @time 2024-02-23 23:16:10
+ * @description 策略模式
+ */
+public class StrategyPattern {
+
+    public static void main(String[] args) {
+
+
+    }
+}
